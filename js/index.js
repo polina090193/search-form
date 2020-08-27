ab55fa24c30e081a9acf6fdf758547efc36d5d53
polina090193 <polina_0901@mail.ru>
2020-08-27 12:02:19 +0300

    Builded project

--- js/index.js
+++ js/index.js
@@ -1,74 +1,70 @@
+"use strict";
+
+var item = document.querySelector('.item'),
+    itemsWrapper = document.querySelector('.items-wrapper');
+
 function addItem(itemsQuantity) {
-    let item = document.querySelector('.item');
-    let itemsWrapper = document.querySelector('.items-wrapper')
-    
-    for(let i = 1; i < itemsQuantity; i++) {
-        let nextItem = item.cloneNode(true);
-        itemsWrapper.append(nextItem)
+  for (var i = 1; i < itemsQuantity; i++) {
+    var nextItem = item.cloneNode(true);
+    itemsWrapper.appendChild(nextItem);
+  }
+}
+
+addItem(6);
+document.querySelector('.see-more-items').addEventListener('click', function () {
+  for (var i = 0; i < 3; i++) {
+    var nextItem = item.cloneNode(true);
+    itemsWrapper.appendChild(nextItem);
+  }
+});
+document.querySelector('.see-more-additions').addEventListener('click', function () {
+  document.querySelector('.hidden-additions').style.display = 'block';
+  document.querySelector('.see-more-additions').style.display = 'none';
+});
+var filterBlocks = document.querySelectorAll('.filter__block');
+
+var _loop = function _loop(i) {
+  var filteritemHeader = filterBlocks[i].querySelector('.filter__item-header'),
+      filterBody = filterBlocks[i].querySelector('.filter__body'),
+      showHideButton = filterBlocks[i].querySelector('.show-hide-button');
+  filteritemHeader.addEventListener('click', function () {
+    if (filterBody.style.display === 'none') {
+      filterBody.style.display = '';
+    } else {
+      filterBody.style.display = 'none';
     }
-}
-addItem(6)
 
-function addMoreItems() {
-    let item = document.querySelector('.item');
-    let itemsWrapper = document.querySelector('.items-wrapper')
-    
-    for(let i = 0; i < 3; i++) {
-        let nextItem = item.cloneNode(true);
-        itemsWrapper.append(nextItem)
+    if (showHideButton.style.transform === 'rotate(180deg)') {
+      showHideButton.style.transform = '';
+    } else {
+      showHideButton.style.transform = 'rotate(180deg)';
     }
+  });
+};
+
+for (var i = 0; i < filterBlocks.length; i++) {
+  _loop(i);
 }
 
-document.querySelector('.see-more-items')
-        .addEventListener('click', addMoreItems)
+var buttonSubwayAll = document.querySelector('.input_subway-all'),
+    subwayDistanceButtons = document.querySelectorAll('.input_subway-distance');
 
-
-
-document.querySelector('.see-more-additions')
-        .addEventListener('click', showHiddenAdditions)
-
-function showHiddenAdditions() {
-    document.querySelector('.hidden-additions')
-            .style.display = 'block';
-    document.querySelector('.see-more-additions')
-            .style.display = 'none';
+for (var _i = 0; _i < subwayDistanceButtons.length; _i++) {
+  subwayDistanceButtons[_i].addEventListener('click', function () {
+    buttonSubwayAll.checked = false;
+  });
 }
 
-
-
-let filterBlocks = document.querySelectorAll('.filter__block'), filterBlock;
-for (filterBlock of filterBlocks) {
-    let filteritemHeader = filterBlock.querySelector('.filter__item-header'),
-        filterBody =       filterBlock.querySelector('.filter__body'),
-        showHideButton =   filterBlock.querySelector('.show-hide-button');
-    
-    filteritemHeader.addEventListener('click', () => {
-        filterBody.style.display = 
-        filterBody.style.display === 'none' ? '' : 'none';
-
-        showHideButton.style.transform =
-        showHideButton.style.transform === 'rotate(180deg)' ?
-        '' : 'rotate(180deg)';
-        }
-    )
-}      
-
-
-let buttonSubwayAll = document.querySelector('.input_subway-all'),
-    subwayDistanceButtons = document.querySelectorAll('.input_subway-distance'),
-    sdb;
-
-for (sdb of subwayDistanceButtons) {
-    sdb.addEventListener('click', () => {
-        buttonSubwayAll.checked = false
-    })
-}
-
-buttonSubwayAll.addEventListener('click', () => {
-    for (sdb of subwayDistanceButtons) {
-        if(sdb.checked == true) sdb.checked = false;
-        sdb.addEventListener('click', () => {
-            buttonSubwayAll.checked = false
-         })
+buttonSubwayAll.addEventListener('click', function () {
+  for (var _i2 = 0; _i2 < subwayDistanceButtons.length; _i2++) {
+    if (subwayDistanceButtons[_i2].checked == true) {
+      subwayDistanceButtons[_i2].checked = false;
     }
-})
+
+    ;
+
+    subwayDistanceButtons[_i2].addEventListener('click', function () {
+      buttonSubwayAll.checked = false;
+    });
+  }
+});
